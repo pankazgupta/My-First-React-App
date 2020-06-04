e98e5b3f9717043df5950f7e04dcbe3b564b117e
Pankaj Kumar Gupta <pankajgupta18700.pg@gmail.com>
2020-06-04 17:31:38 +0530

    Updates

---
+++ precache-manifest.96f696bf342b67e8cafe317b814a4368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a735b27b69ea57a3865",
+    "url": "/My-First-React-App/static/css/main.411562b4.chunk.css"
+  },
+  {
+    "revision": "4a735b27b69ea57a3865",
+    "url": "/My-First-React-App/static/js/main.a35bf72e.chunk.js"
+  },
+  {
+    "revision": "0ceedb1d1801976740d0",
+    "url": "/My-First-React-App/static/js/runtime~main.920761a4.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/My-First-React-App/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "ce02dc0bd95d80dad889767c1350278a",
+    "url": "/My-First-React-App/static/media/pankaj.ce02dc0b.JPG"
+  },
+  {
+    "revision": "276d5b0f306cfa10e561fc6c425a6994",
+    "url": "/My-First-React-App/index.html"
+  }
+];
